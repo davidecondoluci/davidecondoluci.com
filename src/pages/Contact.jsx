673a3ea7eca37932eac00a511bfb85ce3a213ea9
davidecondoluci <151521993+davidecondoluci@users.noreply.github.com>
2024-06-21 00:18:35 +0200

    fix contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -30,97 +30,97 @@ const Contact = () => {
           Get in touch or shoot me an email directly on
           davide.condoluci1@gmail.com
         </motion.p>
-        <motion.div
-          initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, ease: "easeInOut", delay: 0.4 }}
-          className="flex flex-col items-start w-full max-w-2xl"
-        >
-          <form
-            className="w-full space-y-4"
-            action="https://api.staticforms.xyz/submit"
-            method="post"
-          >
-            <input
-              type="hidden"
-              name="accessKey"
-              value="4e32a4ad-ff35-4d56-b95d-ea3129bad48c"
-            />
-            <input type="text" name="honeypot" className="hidden" />
-            <input
-              className="appearance-none border rounded w-full py-2 px-4 leading-tight focus:outline-none font-sans font-light border-lightgray focus:border-green bg-white hover:bg-white"
-              id="inline-full-name"
-              type="text"
-              name="name"
-              placeholder="Full Name"
-            />
-            <input
-              className="appearance-none border rounded w-full py-2 px-4 leading-tight focus:outline-none font-sans font-light border-lightgray focus:border-green bg-white hover:bg-white"
-              id="inline-email"
-              type="email"
-              name="email"
-              placeholder="Email"
-            />
-            <textarea
-              className="appearance-none border rounded w-full h-48 py-2 px-4 leading-tight focus:outline-none font-sans font-light border-lightgray focus:border-green bg-white hover:bg-white"
-              id="inline-message"
-              placeholder="Message"
-              name="message"
-            />
-            <motion.button
-              whileHover={{ scale: 1.05 }}
-              className={`shadow bg-lightgray hover:bg-green text-white focus:shadow-outline focus:outline-none py-2 px-6 rounded font-sans font-medium`}
-              type="submit"
-            >
-              Send
-            </motion.button>
-          </form>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, ease: "easeInOut", delay: 0.6 }}
-          className="flex flex-col md:flex-row lg:flex-row w-full md:items-center lg:items-center space-y-4 md:space-y-0 lg:space-y-0 space-x-2"
-        >
-          <h2 className="font-sans font-light text-left lg:items-center md:mr-4 lg:mr-4">
-            Stay connected with me:
-          </h2>
-          <div className="flex flex-row lg:items-center space-x-4">
-            <motion.a
-              whileHover={{ scale: 1.2 }}
-              href="https://github.com/davidecondoluci"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaGithub className="text-[#333] text-3xl" />
-            </motion.a>
-            <motion.a
-              whileHover={{ scale: 1.2 }}
-              href="https://www.linkedin.com/in/davide-condoluci/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaLinkedin className="text-[#0077B5] text-3xl" />
-            </motion.a>
-            <motion.a
-              whileHover={{ scale: 1.2 }}
-              href="https://www.instagram.com/davide_condoluci/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaInstagram className="text-[#E4405F] text-3xl" />
-            </motion.a>
-            <motion.a
-              whileHover={{ scale: 1.2 }}
-              href="https://x.com/davidecondoluci"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaXTwitter className="text-[#000000] text-3xl" />
-            </motion.a>
-          </div>
-        </motion.div>
       </div>
+      <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, ease: "easeInOut", delay: 0.4 }}
+        className="flex flex-col justify-center items-start w-full max-w-2xl"
+      >
+        <form
+          className="w-full space-y-4 pb-4"
+          action="https://api.staticforms.xyz/submit"
+          method="post"
+        >
+          <input
+            type="hidden"
+            name="accessKey"
+            value="4e32a4ad-ff35-4d56-b95d-ea3129bad48c"
+          />
+          <input type="text" name="honeypot" className="hidden" />
+          <input
+            className="appearance-none border rounded w-full py-2 px-4 leading-tight focus:outline-none font-sans font-light border-lightgray focus:border-green bg-white hover:bg-white"
+            id="inline-full-name"
+            type="text"
+            name="name"
+            placeholder="Full Name"
+          />
+          <input
+            className="appearance-none border rounded w-full py-2 px-4 leading-tight focus:outline-none font-sans font-light border-lightgray focus:border-green bg-white hover:bg-white"
+            id="inline-email"
+            type="email"
+            name="email"
+            placeholder="Email"
+          />
+          <textarea
+            className="appearance-none border rounded w-full h-48 py-2 px-4 leading-tight focus:outline-none font-sans font-light border-lightgray focus:border-green bg-white hover:bg-white"
+            id="inline-message"
+            placeholder="Message"
+            name="message"
+          />
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            className={`shadow bg-lightgray hover:bg-green text-white focus:shadow-outline focus:outline-none py-2 px-6 rounded font-sans font-medium`}
+            type="submit"
+          >
+            Send
+          </motion.button>
+        </form>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, ease: "easeInOut", delay: 0.6 }}
+        className="flex flex-col md:flex-row lg:flex-row w-full max-w-2xl justify-start md:items-center lg:items-center space-y-2 md:space-x-2 lg:space-x-2"
+      >
+        <h2 className="font-sans font-light text-left lg:items-center md:mr-4 lg:mr-4">
+          Stay connected with me:
+        </h2>
+        <div className="flex flex-row lg:items-center space-x-4">
+          <motion.a
+            whileHover={{ scale: 1.2 }}
+            href="https://github.com/davidecondoluci"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub className="text-[#333] text-3xl" />
+          </motion.a>
+          <motion.a
+            whileHover={{ scale: 1.2 }}
+            href="https://www.linkedin.com/in/davide-condoluci/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin className="text-[#0077B5] text-3xl" />
+          </motion.a>
+          <motion.a
+            whileHover={{ scale: 1.2 }}
+            href="https://www.instagram.com/davide_condoluci/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram className="text-[#E4405F] text-3xl" />
+          </motion.a>
+          <motion.a
+            whileHover={{ scale: 1.2 }}
+            href="https://x.com/davidecondoluci"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaXTwitter className="text-[#000000] text-3xl" />
+          </motion.a>
+        </div>
+      </motion.div>
     </motion.div>
   );
 };
