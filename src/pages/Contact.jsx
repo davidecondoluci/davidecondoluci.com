744ddd611c9999af727806dc11bb4c59fff05265
davidecondoluci <151521993+davidecondoluci@users.noreply.github.com>
2025-07-07 19:15:01 +0200

    delete contact h-screen and fixed pb

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -82,7 +82,7 @@ const Contact = () => {
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -20 }}
       transition={{ duration: 0.5, ease: "easeInOut" }}
-      className="flex flex-col items-center px-4 pt-36 pb-36 space-y-4 h-screen"
+      className="flex flex-col items-center px-4 pt-36 pb-16 space-y-4"
     >
       <div className="flex flex-col justify-center items-center space-y-8">
         <motion.h1
